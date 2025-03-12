0941417a723a4fe091f0def91624a28b90e9f018
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2025-03-12 21:59:44 +0900

    Initial Commit

---
+++ src/main/java/com/example/noticehub/NoticeHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.noticehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeHubApplication.class, args);
+    }
+
+}
